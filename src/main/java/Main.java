6096b69c0faa0747617484ba5a2c554e0b174df6
Roman Bulinski <aboandrus@gmail.com>
2019-08-17 08:46:37 +0200

    Created structure of app

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+
+    public static double spiderToFly(final String spider, final String fly) {
+        // Your code here
+        return 0d;
+    }
+
+
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void example() {
+        assertEquals(4.63522d, Main.spiderToFly("H3", "E2"), 1e-3d);
+    }
+
+    @Test
+    public void same() {
+        assertEquals(0d, Main.spiderToFly("C2", "C2"), 1e-3d);
+        assertEquals(0d, Main.spiderToFly("A0", "A0"), 1e-3d);
+        assertEquals(0d, Main.spiderToFly("F3", "F3"), 1e-3d);
+    }
+
+    @Test
+    public void radialOut() {
+        assertEquals(1d, Main.spiderToFly("H1", "H2"), 1e-3d);
+        assertEquals(3d, Main.spiderToFly("H1", "H4"), 1e-3d);
+    }
+
+    @Test
+    public void radialThruTheMiddle() {
+        assertEquals(8d, Main.spiderToFly("G4", "C4"), 1e-3d);
+        assertEquals(7d, Main.spiderToFly("G4", "C3"), 1e-3d);
+    }
+
+
+}
